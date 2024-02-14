e726db5d38101ce3f0b219036131227eb7585302
Arabov Denys <tanden8888@gmail.com>
2024-02-14 21:49:15 +0200

    Test1

--- src/main/java/ua/hillel/arabov/lessons/lesson3/Main.java
+++ src/main/java/ua/hillel/arabov/lessons/lesson3/Main.java
@@ -2,7 +2,7 @@ package ua.hillel.arabov.lessons.lesson3;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello GIT!!!");
+        System.out.println("Hello GIT");
 
     }
 }
